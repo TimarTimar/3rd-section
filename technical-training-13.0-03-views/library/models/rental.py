3d9c84d39b8a5b1e9803f01d37305061d71ac436
TimarTimar <tamasbelinszky@gmail.com>
2020-06-05 04:54:57 +0200

    3rd section finished

--- technical-training-13.0-03-views/library/models/rental.py
+++ technical-training-13.0-03-views/library/models/rental.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from odoo import fields, models
+from odoo import fields, models, api
+import time
 
 
 class Rentals(models.Model):
@@ -9,8 +10,8 @@ class Rentals(models.Model):
     customer_id = fields.Many2one('library.partner', string='Customer')
     book_id = fields.Many2one('library.book', string='Book')
 
-    rental_date = fields.Date()
-    return_date = fields.Date()
+    rental_date = fields.Date(required=True)
+    return_date = fields.Date(required=True)
 
     customer_address = fields.Text(related='customer_id.address')
     customer_email = fields.Char(related='customer_id.email')
@@ -18,3 +19,13 @@ class Rentals(models.Model):
     book_authors = fields.Many2many(related='book_id.author_ids')
     book_edition_date = fields.Date(related='book_id.edition_date')
     book_publisher = fields.Many2one(related='book_id.publisher_id')
+    state = fields.Selection([('open', "Open"), ('closed', 'Closed'), ('late', 'Late')], default='open')
+    date = fields.Date(default=fields.Date.today)
+
+    """@api.depends('return_date')
+    def _islate(self):
+        for r in self:
+            if r.date>=r.return_date:
+                r.state='late'
+            else:
+                pass"""
